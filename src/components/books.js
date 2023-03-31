d88662780b84b86e73cde445774a686f4c9d745b
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-03-31 05:45:40 -0300

    Refactor in more components

--- src/components/books.js
+++ src/components/books.js
@@ -1,19 +1,15 @@
 import { useState } from 'react';
+import BookList from './booklist';
+import NewBookForm from './newbookform';
 
 function Books() {
   const [books, setBooks] = useState([]);
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const newBook = { title, author };
+  const handleAddBook = (newBook) => {
     setBooks([...books, newBook]);
-    setTitle('');
-    setAuthor('');
   };
 
-  const handleDelete = (index) => {
+  const handleDeleteBook = (index) => {
     const updatedBooks = [...books];
     updatedBooks.splice(index, 1);
     setBooks(updatedBooks);
@@ -22,29 +18,9 @@ function Books() {
   return (
     <div>
       <h1>Books:</h1>
-      <ul>
-        {books.map((book, index) => (
-          <li key={`${book.title}-${book.author}`}>
-            {book.title}
-            {' '}
-            by
-            {book.author}
-            <button type="button" onClick={() => handleDelete(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      <BookList books={books} onDelete={handleDeleteBook} />
       <h2>Add a book:</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title-input">
-          Title:
-          <input id="title-input" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-        </label>
-        <label htmlFor="author-input">
-          Author:
-          <input id="author-input" type="text" value={author} onChange={(e) => setAuthor(e.target.value)} />
-        </label>
-        <button type="submit">Add Book</button>
-      </form>
+      <NewBookForm onSubmit={handleAddBook} />
     </div>
   );
 }
