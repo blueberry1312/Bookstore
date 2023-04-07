0a644c41fafa0247b3aa1839d237526827390f8d
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-04-07 13:58:19 -0400

    Update to Navlink for better ux

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,17 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './navbar.css';
 import Logo from '../assets/icon.png';
 
 function Navbar() {
   return (
     <nav className="nav-bar">
-      <Link className="nav-brand" to="/">Bookstore CMS</Link>
+      <NavLink className="nav-brand" to="/">Bookstore CMS</NavLink>
       <ul className="nav-links">
         <li className="place-link">
-          <Link className="nav-link" to="/">BOOKS</Link>
+          <NavLink className="nav-link" to="/" activeClassName="active-link">BOOKS</NavLink>
         </li>
         <li className="place-link">
-          <Link className="nav-link" to="/categories">CATEGORIES</Link>
+          <NavLink className="nav-link" to="/categories" activeClassName="active-link">CATEGORIES</NavLink>
         </li>
         <li className="place-icon">
           <button className="icon-button" type="button">
